e62b74c87770d0385f6dfd11a7e12648e570a6d1
martinivnv <martinivnv2002@gmail.com>
2022-01-08 20:37:40 -0500

    Added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Post from "./Post";
 
 function App() {
 	return (
 		<div className="app">
-			<div className="container"></div>
+			<header>
+				<h1 id="app-title">🌙 Spacestagram</h1>
+				<h4 id="app-subtitle">
+					Powered by NASA's Astronomy Picture of the Day API
+				</h4>
+			</header>
+			<div className="container">
+				<Post />
+			</div>
 		</div>
 	);
 }
